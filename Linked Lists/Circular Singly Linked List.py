f0f4e88fbe11cf315a0e954b4e956eb156376dc9
Tushar Chand <tushar.k.chand@gmail.com>
2023-12-07 21:16:04 -0500

    Added Deletion and Print List functionality to Circular Singly Linked List

--- Linked Lists/Circular Singly Linked List.py
+++ Linked Lists/Circular Singly Linked List.py
@@ -24,3 +24,48 @@ class CircularLinkedList:
 
         current.next = new_node
         new_node.next = self.head
+
+    def deleteNode(self, key):
+        if self.head is None:
+            return
+        
+        last = self.head
+        
+        #if the list only contains single element
+        if self.head.next == self.head and self.head.data == key:
+            self.head = None
+
+        #if the list has more than 1 element and the first element is the key
+        if self.head.data == key:
+            current = self.head
+            
+            while current.next != self.head:
+                current = current.next
+
+            current.next = self.head.next
+            self.head = self.head.next
+            
+            return
+
+        # Either the node to be deleted is 
+        # not found or the end of list 
+        # is not reached 
+        while last.next != self.head and last.next.data != key:
+            last = last.next
+        
+        if last.next.data == key:
+            d = last.next
+            last.next = d.next
+            d = None
+        else:
+            print('Node not found')
+            return
+
+    def printList(self):
+        current = self.head
+        if self.head is None:
+            print('Empty List')
+            return
+        while current.next != self.head:
+            print(current.data)
+            current = current.next
