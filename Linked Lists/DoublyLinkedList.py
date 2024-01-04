f3e71fb0453c967f671de46b202f6a29f5e315e9
Tushar Chand <tushar.k.chand@gmail.com>
2024-01-04 17:12:18 -0500

    Added Remove Node and Remove Duplicates

--- Linked Lists/DoublyLinkedList.py
+++ Linked Lists/DoublyLinkedList.py
@@ -105,6 +105,60 @@ class DoublyLinkedList:
                     return
             cur_node = cur_node.next
 
+    def remove_duplicates(self):
+        cur_node = self.head
+        dup_values = dict()
+        prev = None
+
+        while cur_node:
+            if cur_node.data in dup_values:
+                prev.next = cur_node.next
+                self.delete_node(cur_node) #Alt: cur_node = None
+            else:
+                dup_values[cur_node.data] = 1
+                prev = cur_node
+            cur_node = prev.next
+
+    def delete_node(self, node):
+        cur = self.head
+        while cur:
+            if cur == node and cur == self.head:
+                # Case 1:
+                if not cur.next:
+                    cur = None
+                    self.head = None
+                    return
+
+                # Case 2:
+                else:
+                    nxt = cur.next
+                    cur.next = None
+                    nxt.prev = None
+                    cur = None
+                    self.head = nxt
+                    return
+
+            elif cur == node:
+                # Case 3:
+                if cur.next:
+                    nxt = cur.next
+                    prev = cur.prev
+                    prev.next = nxt
+                    nxt.prev = prev
+                    cur.next = None
+                    cur.prev = None
+                    cur = None
+                    return
+
+                # Case 4:
+                else:
+                    prev = cur.prev
+                    prev.next = None
+                    cur.prev = None
+                    cur = None
+                    return
+            cur = cur.next
+
     def reverse(self):
         temp = None
         cur_node = self.head
@@ -148,10 +202,26 @@ class DoublyLinkedList:
 # dllist.print_list()
 
 #Test for Reversing the List
+# dllist = DoublyLinkedList()
+# dllist.append(1)
+# dllist.append(2)
+# dllist.append(3)
+# dllist.append(4)
+# dllist.reverse()
+# dllist.print_list()
+
+#Test Delete Duplicate
 dllist = DoublyLinkedList()
-dllist.append(1)
-dllist.append(2)
-dllist.append(3)
+dllist.append(8)
 dllist.append(4)
-dllist.reverse()
+dllist.append(4)
+dllist.append(6)
+dllist.append(4)
+dllist.append(8)
+dllist.append(4)
+dllist.append(10)
+dllist.append(12)
+dllist.append(12)
+
+dllist.remove_duplicates()
 dllist.print_list()
