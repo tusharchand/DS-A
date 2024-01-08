af88d0803871b134eea015c8dacfcfad9aee4e5e
Tushar Chand <tushar.k.chand@gmail.com>
2024-01-08 15:46:10 -0500

    Added another approach for Two Sum using Hash Table which takes O(n) Time Complexity and O(n) Space Complexity

--- Arrays/Two Sum Problem.py
+++ Arrays/Two Sum Problem.py
@@ -17,3 +17,18 @@ print(len(A)-1)
 print(two_sum_brute_force(A,target))
 target = 20
 print(two_sum_brute_force(A,target))
+
+def two_sum_hash_table(A, target):
+    ht = dict()
+    for i in range(len(A)):
+        if A[i] in ht:
+            print(A[i], ht[A[i]])
+            return True
+        else:
+            ht[target - A[i]] = A[i]
+    return False
+
+A = [-2, 1, 2, 4, 7, 11]
+target = 13
+
+print(two_sum_hash_table(A,target))
