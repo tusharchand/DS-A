5af11372d03b77e0d9f3d9cce95111970ff556cc
Tushar Chand <tushar.k.chand@gmail.com>
2024-01-08 15:38:26 -0500

    Added Two Sum Problem with Brute Force, n^2, time complexity

---
+++ Arrays/Two Sum Problem.py
@@ -0,0 +1,19 @@
+#Two Sum Problem
+
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
+def two_sum_brute_force(A, target):
+    for i in range(len(A)-1):
+        for j in range(i+1, len(A)):
+            if A[i] + A[j] == target:
+                print(A[i], A[j])
+                return True
+    return False
+
+A = [-2, 1, 2, 4, 7, 11]
+target = 13
+print(len(A))
+print(len(A)-1)
+print(two_sum_brute_force(A,target))
+target = 20
+print(two_sum_brute_force(A,target))
