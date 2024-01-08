b48431d7b77c5b7a428f5bccfd4c2bba70f9036c
Tushar Chand <tushar.k.chand@gmail.com>
2024-01-08 15:50:45 -0500

    Added best approach for Two Sum utilizing O(n) Time Complexity and O(1) Space Complexity

--- Arrays/Two Sum Problem.py
+++ Arrays/Two Sum Problem.py
@@ -32,3 +32,22 @@ A = [-2, 1, 2, 4, 7, 11]
 target = 13
 
 print(two_sum_hash_table(A,target))
+
+def two_sum(A, target):
+    i = 0
+    j = len(A)-1
+    
+    while i < j:
+        if A[i] + A[j] == target:
+            print(A[i], A[j])
+            return True
+        elif A[i] + A[j] < target:
+            i += 1
+        else:
+            j -= 1
+    return False
+
+A = [-2, 1, 2, 4, 7, 11]
+target = 13
+
+print(two_sum(A,target))
